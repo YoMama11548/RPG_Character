ed123fe246e51eef488d690362ba73addcf2a540
22002545CTC <22002545CTC@MA-CTC-INST-S19>
2023-03-09 10:25:27 -0500

    Add project files.

---
+++ RPG_Character/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RPG_Character
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+
+    
+    
+    public partial class MainWindow : Window
+    {
+
+        private RPGChar characterSheet = new RPGChar();
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = characterSheet;
+        }
+
+        private void ButtonUpdate_Click(object sender, RoutedEventArgs e)
+        {
+            // Update name
+            characterSheet.Name = textBoxName.Text;
+            MessageBox.Show(characterSheet.Name);
+
+        }
+
+        private void REROLL_Click(object sender, RoutedEventArgs e)
+        {
+            characterSheet.Roll();
+            updateStats();
+        }
+
+        private void updateStats()
+        {
+            textSTR.Text = characterSheet.Strength.ToString();
+            textDEX.Text = characterSheet.Dexterity.ToString();
+            textINT.Text = characterSheet.Intellegence.ToString();
+            textCHR.Text = characterSheet.Charisma.ToString();
+            textWIS.Text = characterSheet.Wisdom.ToString();
+            textLUC.Text = characterSheet.Luck.ToString();
+        }
+    }
+}
---
+++ RPG_Character/RPGChar.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPG_Character
+{
+    internal class RPGChar
+    {
+        #region Fields
+        //Fields
+        private CharacterClasses _characterClass;
+        private int _charisma;
+        private int _maxCharisma = 20;
+        private int _dexterity;
+        private int _maxDexterity = 20;
+        private int _intellegence;
+        private int _maxIntellegence = 20;
+        private int _luck;
+        private int _maxLuck = 20;
+        private string _name;
+        private int _strength;
+        private int _maxStrength = 20;
+        private int _wisdom;
+        private int _maxWisdom = 20;
+        #endregion Fields
+
+
+
+        private Random _rng = new Random();
+
+        #region Properties
+        //Properties
+        public CharacterClasses ChacterClass
+        {
+            get {  return _characterClass;}
+            set { _characterClass = value; }
+        }
+        public int Charisma { get { return _charisma; } }
+        public int Dexterity { get { return _dexterity; } }
+        public int Intellegence { get { return _intellegence; } }
+        public int Level { get; set; }
+        public int Luck { get { return _luck; } }
+        public string Name { get; set; }
+        public int Strength { get { return _strength; } }
+        public int Wisdom { get { return _wisdom; } }
+        #endregion Properties
+
+        public RPGChar()
+        {
+            Roll();
+        }
+
+        public void Roll()
+        {
+            _charisma = _rng.Next(1, _maxCharisma + 1);
+            _dexterity = _rng.Next(1, _maxDexterity + 1);
+            _intellegence = _rng.Next(1, _maxIntellegence + 1);
+            _luck = _rng.Next(1, _maxLuck + 1);
+            _strength = _rng.Next(1, _maxStrength + 1);
+            _wisdom = _rng.Next(1, _maxWisdom + 1);
+        }
+    }
+
+    public enum CharacterClasses
+    {
+        None = 0,
+        Cleric,
+        Rouge,
+        Barbarian,
+        Wizard
+    }
+}
